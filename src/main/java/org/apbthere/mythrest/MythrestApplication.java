f77558a0ae4839d811b112f707476e8505d9a37a
Alexander Baiko <abcconsultingit@gmail.com>
2016-09-30 17:32:43 -0400

    1: create REST interface for Myth Frontend
    Task-Url: http://github.com/apbthere/mythrest/issues/1

---
+++ src/main/java/org/apbthere/mythrest/MythrestApplication.java
@@ -0,0 +1,70 @@
+package org.apbthere.mythrest;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MythrestApplication {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythrestApplication.class, args);
+	}
+
+	@RequestMapping(value = "/control/{frontendIp}/{port}", method = RequestMethod.GET)
+	public Object control(@PathVariable String frontendIp, @PathVariable int port, String command) {
+		String reply = null;
+		InetSocketAddress sockAdr = new InetSocketAddress(frontendIp, port);
+
+		try (Socket sSocket = new Socket()) {
+			sSocket.connect(sockAdr, 1000);
+
+			try (OutputStream outputStream = sSocket.getOutputStream()) {
+				PrintWriter sOutputStream = new PrintWriter(outputStream);
+
+				logger.info("Sending command [{}]", command);
+				sOutputStream.println(command);
+				sOutputStream.flush();
+				
+				try (InputStream inputStream = sSocket.getInputStream()) {
+					BufferedReader sInputStream = new BufferedReader(new InputStreamReader(inputStream));
+					reply = sInputStream.readLine();
+
+					sInputStream.close();
+					inputStream.close();
+				}
+				
+				sOutputStream.close();
+				outputStream.close();
+			}
+
+			sSocket.close();
+		} catch (IOException e1) {
+			logger.error("Catching", e1);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e1.getMessage());
+		}
+		return reply;
+	}
+}
---
+++ src/test/java/org/apbthere/mythrest/MythrestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.apbthere.mythrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MythrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
